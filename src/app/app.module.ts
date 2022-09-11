3bf0f6dd2b563ac45e38341e82b0377f0322ecbf
FathimaZahrah <zahrahshafik13@gmail.com>
2022-09-11 11:15:18 +0530

    Basic front end pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupteacherComponent } from './signupteacher/signupteacher.component';
+import { SignupstudentComponent } from './signupstudent/signupstudent.component';
+import { TeacherhomeComponent } from './teacherhome/teacherhome.component';
+import { StudenthomeComponent } from './studenthome/studenthome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SignupteacherComponent,
+    SignupstudentComponent,
+    TeacherhomeComponent,
+    StudenthomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/signupstudent/signupstudent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signupstudent',
+  templateUrl: './signupstudent.component.html',
+  styleUrls: ['./signupstudent.component.css']
+})
+export class SignupstudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
